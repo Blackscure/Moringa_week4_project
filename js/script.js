fa66e4cbd15769e9b58f9ec0c9c80c97d4e2db6e
Blackscure <Wekesabuyahi@gmail.com>
2020-03-01 18:32:44 +0300

    script filder added

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// DATA COLLECTION FROM THE ORDER FORM
+var pizzaSize, crust, toppings, subTotalPrice
+
+function Order(pizzaSize, crust, toppings, subTotalPrice){
+
+  this.size = pizzaSize;
+  this.crust = crust;
+  this.toppings = toppings;
+  this.subTotalPrice =subTotalPrice;
+}
+
+ 
